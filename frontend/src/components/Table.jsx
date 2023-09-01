d5440e1f54d46184196cfe01ce0af625df7da729
Artiom Tsimkin <artiomtz@gmail.com>
2023-09-01 19:55:20 -0400

    feat: add employee table

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,4 +1,5 @@
 import { React, useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
 import EmployeeContext from "../context/EmployeeContext";
 
 export default function Table() {
@@ -9,10 +10,34 @@ export default function Table() {
   }, []);
 
   return (
-    <div>
-      {employees.map((employee) => (
-        <div key={employee.firstName}>{employee.firstName}</div>
-      ))}
-    </div>
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">First Name</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">Salary</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {employees.map((employee) => (
+          <tr key={employee.id}>
+            <td>{employee.firstName}</td>
+            <td>{employee.lastName}</td>
+            <td>{employee.salary}</td>
+            <td>
+              <Link to={"/" + employee.id}>
+                <button type="button" className="btn btn-primary">
+                  Edit
+                </button>
+              </Link>
+              <button type="button" className="btn btn-danger">
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
