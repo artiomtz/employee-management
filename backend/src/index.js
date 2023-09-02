a4c240e2601a9d0feaf49f9d542e5cebf94a96b7
Artiom Tsimkin <artiomtz@gmail.com>
2023-09-02 01:48:35 -0400

    fix: add cors module

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
 const employeeRouter = require("./routes/employees");
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,HEAD,PUT,POST,DELETE",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
 app.use("/employees", employeeRouter);
 app.listen(3000);
